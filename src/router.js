910c76b90f93798dffda3f7980377ff7696c1de6
Brandon Bailey <brandon.bailey1987@gmail.com>
2019-06-13 12:49:04 -0500

    Add login functionality

--- src/router.js
+++ src/router.js
@@ -1,26 +1,42 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import About from './views/About.vue'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
-  mode: "history",
-  base: process.env.BASE_URL,
+  mode: 'history', // Required for Adal library
   routes: [
     {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'home',
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: '/about',
+      name: 'about',
+      component: About
+    },
+    {
+      path: '/secret',
+      name: 'secret',
+      component: About,
+      meta: {
+        requireAuth: true
+      }
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: About,
+      meta: {
+        requireAuth: true,
+        requireRoles: [ // Much match at least one of these roles
+          'TheMan',
+          'Admin'
+        ]
+      }
     }
   ]
-});
+})
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld :msg='msg'/>
+    <button v-on:click="$adal.login()" >Sign In</button>
+    <button v-on:click="$adal.logout()" >Sign Out</button>
+    <div v-if="$adal.isAuthenticated()">You are signed in!</div>
+    <div v-if="$adal.checkRoles(['Admin'])">You're also an admin</div>
+    <div v-if="!$adal.checkRoles(['Admin'])">You're not an admin</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: "home",
+  name: 'home',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      msg: "Signing in..."
+    }
+  },
+  async created () {
+    if (this.$adal.isAuthenticated()) {
+      this.msg = `Hello, ${this.$adal.user.profile.name}`  
+
+      let userInfo = await this.getUserInfo()
+      console.log('userInfo', userInfo)
+      this.msg += '. It looks like your mail nickname is ' + userInfo.mailNickname + ' according to the Graph API'
+    } else {
+      this.msg = "Please sign in"
+    }
+  },
+
+  methods: {
+    async getUserInfo () {
+      console.log('graphApi', this.$graphApi);
+      let res = await this.$graphApi.get(`me`, {
+        params: {
+          'api-version': 1.6
+        }
+      })
+      console.log('getUserInfo', res)
+      return res.data
+    }
   }
-};
+}
 </script>
